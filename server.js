e8aecab4458cd8246b3cf457babbbae9901ad2e3
JoeAhava <45706881+JoeAhava@users.noreply.github.com>
2020-08-13 05:41:27 +0300

    final

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 
-// require and use "multer"...
+const upload = multer();
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+
+  try {
+    res.json({
+      name: req.file.originalname,
+      size: req.file.size
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: "Some error occured", statusCode: 500 })
+  }
+
+
 });
 
 app.listen(process.env.PORT || 3000, function () {
